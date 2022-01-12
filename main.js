923f584ef44271fcf6f785cc551584c038021cec
李亮 <zkyeu@163.com>
2022-01-13 00:51:53 +0800

    封装请求

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 import App from './App'
+import request from 'common/request.js'
+
 
 // #ifndef VUE3
 import Vue from 'vue'
 Vue.config.productionTip = false
+Vue.prototype.Http = request;
 App.mpType = 'app'
 const app = new Vue({
     ...App
